377abc5ba1a869ead942dd448dbcd6f9939ed9b5
Zachary Herberger <zherberger@gmail.com>
2025-03-22 20:07:39 -0400

    Add project for This Website

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GitHubLink, Page, Stack } from './components';
 import beadProPenguinOriginal from './assets/bead-pro-penguin-original.png';
 import beadProPenguinScreenshot from './assets/bead-pro-penguin-screenshot.png';
 import botn1 from './assets/botn-1.png';
 import botn2 from './assets/botn-2.png';
 import botn3 from './assets/botn-3.png';
-import myHomePage from './assets/my-home-page.png';
+import myHomePageDark from './assets/my-home-page.png';
+import myHomePageLight from './assets/my-home-page-light.png';
+import { ThemeContext } from "./main";
 
 function Project({
     name,
@@ -62,8 +64,18 @@ function Project({
 }
 
 export default function Projects() {
+    const themeContext = useContext(ThemeContext);
+
     return (
         <Page>
+            <Project
+                name="This Website"
+                url="https://github.com/zherberger/my-home-page"
+                description={"Woah, meta. I worked on this as a sort of \"capstone\" after completing my AWS Solutions Architect certification. It's hosted as a static webpage in S3 and delivered through CloudFront to ensure fast load times."}
+                date="2024"
+                images={themeContext.theme === "dark" ? [myHomePageLight] : [myHomePageDark]}
+            />
+            <br/>
             <Project
                 name="Bead Pro"
                 url="https://github.com/zherberger/bead-pro"
